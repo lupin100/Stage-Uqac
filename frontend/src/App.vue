b92c0f124c3fefa87e475bba9b56ce0dd023f250
Manon KLEIN--POL <manon.kleinpol12@gmail.com>
2026-03-16 23:29:12 -0400

    Config de Vuetify

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,47 +5,13 @@ import AppFooter from './components/AppFooter.vue'
 </script>
 
 <template>
-  <div class="layout">
+  <v-app>
     <AppHeader />
 
-    <header class="navigation">
-      <nav>
-        <ul class="nav-links">
-          <li><RouterLink to="/">Accueil</RouterLink></li>
-          <li><RouterLink to="/a-propos">À propos</RouterLink></li>
-        </ul>
-      </nav>
-    </header>
-
-    <main class="content">
+    <v-main>
       <router-view />
-    </main>
+    </v-main>
 
     <AppFooter />
-  </div>
+  </v-app>
 </template>
-
-<style>
-/* Styles globaux pour toute la page */
-body {
-  margin: 0;
-  font-family: Arial, sans-serif;
-  background-color: #f9f9f9;
-  color: #333;
-  min-width: 100%;
-}
-
-/* Le layout en Flexbox permet de garder le footer toujours en bas de l'écran */
-.layout {
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-}
-
-.content {
-  padding: 3rem 2rem;
-  max-width: 800px;
-  margin: 0 auto;
-  flex: 1; /* Remplit l'espace vide entre le header et le footer */
-}
-</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,23 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import './style.css'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css' // Import des icônes
+
+// Création de l'instance Vuetify
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+// Initialisation de l'appli
 const app = createApp(App)
 
 app.use(router)
+app.use(vuetify) // injecter Vuetify dans Vue
 app.mount('#app')
