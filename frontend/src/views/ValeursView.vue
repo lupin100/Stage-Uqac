5d6a002f91fc0de9a509a9292a1ef425e12db508
Léa Despré-Hildevert <desprehildevertlea@gmail.com>
2026-03-17 21:24:43 -0400

    ajout barre grise langue header

--- frontend/src/views/ValeursView.vue
+++ frontend/src/views/ValeursView.vue
@@ -1,65 +1,109 @@
 <template>
     <v-container class="py-10" max-width="1400">
 
-        <v-row justify="center">
+        <v-row justify="center" >
             <v-col cols="12">
 
-                <h2 class="text-h2 mb-8">
+                <h2 class="text-h2 mb-5">
                     Valeurs
                 </h2>
 
-                <v-list density="compact">
+                <v-list class="bg-transparent" density="compact">
                     <v-list-item>
                         <template #prepend>
                         <div class="square-bullet"></div>
                         </template>
-                        <v-list-item-title>TRANSDISCIPLINARITÉ, AU SERVICE DE L’INNOVATION</v-list-item-title>
+                        <v-list-item-title><strong>TRANSDISCIPLINARITÉ</strong>, AU SERVICE DE L’INNOVATION</v-list-item-title>
                     </v-list-item>
 
                     <v-list-item>
                         <template #prepend>
                         <div class="square-bullet"></div>
                         </template>
-                        <v-list-item-title>L’OUVERTURE, AU SERVICE DE L’ACCESSIBILITÉ</v-list-item-title>
+                        <v-list-item-title><strong>L’OUVERTURE</strong>, AU SERVICE DE L’ACCESSIBILITÉ</v-list-item-title>
                     </v-list-item>
 
                     <v-list-item>
                         <template #prepend>
                         <div class="square-bullet"></div>
                         </template>
-                        <v-list-item-title>CONSCIENCE SOCIALE, AU SERVICE DE L’HUMAIN</v-list-item-title>
+                        <v-list-item-title><strong>CONSCIENCE SOCIALE</strong>, AU SERVICE DE L’HUMAIN</v-list-item-title>
                     </v-list-item>
 
                     <v-list-item>
                         <template #prepend>
                         <div class="square-bullet"></div>
                         </template>
-                        <v-list-item-title>TRANSFERT DES CONNAISSANCES, AU SERVICE DE LA RELÈVE VERS LA DURABILITÉ</v-list-item-title>
+                        <v-list-item-title><strong>TRANSFERT DES CONNAISSANCES</strong>, AU SERVICE DE LA RELÈVE VERS LA DURABILITÉ</v-list-item-title>
                     </v-list-item>
                 </v-list>
 
-                <p class="text-body-1 mb-4">
-                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque 
-                    laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi 
-                    architecto beatae vitae dicta.
-                </p>
+                <h4 class="text-h4 mb-5">
+                    <strong>TRANSDISCIPLINARITÉ</strong>, AU SERVICE DE L’INNOVATION
+                </h4>
 
                 <p class="text-body-1 mb-4">
                     Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque 
                     laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi 
                     architecto beatae vitae dicta.
                 </p>
+                <p class="text-body-1 mb-4">
+                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque 
+                    laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi 
+                    architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas 
+                    sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione 
+                    voluptatem sequi nesciunt. 
+                </p>
+
+                <h4 class="text-h4 mb-5">
+                    <strong>L’OUVERTURE</strong>, AU SERVICE DE L’ACCESSIBILITÉ
+                </h4>
 
                 <p class="text-body-1 mb-4">
                     Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque 
                     laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi 
                     architecto beatae vitae dicta.
                 </p>
+                <p class="text-body-1 mb-4">
+                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque 
+                    laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi 
+                    architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas 
+                    sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione 
+                    voluptatem sequi nesciunt. 
+                </p>
+
+                <h4 class="text-h4 mb-5">
+                    <strong>CONSCIENCE SOCIALE</strong>, AU SERVICE DE L’HUMAIN
+                </h4>
 
                 <p class="text-body-1 mb-4">
                     Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque 
                     laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi 
                     architecto beatae vitae dicta.
+                </p>
+                <p class="text-body-1 mb-4">
+                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque 
+                    laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi 
+                    architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas 
+                    sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione 
+                    voluptatem sequi nesciunt. 
+                </p>
+
+                <h4 class="text-h4 mb-5">
+                    <strong>TRANSFERT DES CONNAISSANCES</strong>, AU SERVICE DE LA RELÈVE VERS LA DURABILIT
+                </h4>
+
+                <p class="text-body-1 mb-4">
+                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque 
+                    laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi 
+                    architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas 
+                    sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione 
+                    voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, 
+                    consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore 
+                    et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum 
+                    exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+                     Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae 
+                     consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
                 </p>
 
             </v-col>
@@ -73,5 +117,6 @@
   width: 8px;
   height: 8px;
   background-color: #9e9e9e;
+  margin-right: 12px;
 }
 </style>
