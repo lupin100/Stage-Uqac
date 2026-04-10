371236f316ba694ed8306b3eddbbe19fed61c420
diarracse <mamediarrac13@gmail.com>
2026-04-10 00:26:59 -0400

    correction Cors

--- backend/src/EventSubscriber/CorsSubscriber.php
+++ backend/src/EventSubscriber/CorsSubscriber.php
@@ -20,7 +20,7 @@ class CorsSubscriber implements EventSubscriberInterface
         $response = $event->getResponse();
 
         // On autorise l'adresse du frontend Vue.js (port 5173)
-        $$response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Origin', '*');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     }
